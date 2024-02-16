63c374910faa4171adc27a1ea1b1b60cc639ed0a
m-eddington <swecide@AMD.babylon5.lan>
2024-02-16 20:36:32 +0100

    small edit

--- api-test.sh
+++ api-test.sh
@@ -8,7 +8,7 @@ VAR2=${2}
 # Get device Id from serialnumber
 IDnr=$(curl https://${Domain}/api/devices?serialNumber=${SN} --user ${Username}:${Password} 2>/dev/null | awk -F '"Id":' '{print substr($2,2,36)}')
 
-# Send message to device with device id gotten
+# Send message to device with device id gotten from previous step
 curl -X POST https://${Domain}/api/devices/${IDnr}/sendmessage  -H 'Content-Type: application/json' -d '{"message": "'"${VAR2}"'"}' --user ${Username}:${Password}
 
 exit 0
